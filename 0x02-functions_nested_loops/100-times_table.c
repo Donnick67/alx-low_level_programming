235a664023ec0288c5cfa0288f0d422e22ec5f7d
Raphael Ude <donnick67@gmail.com>
2022-07-05 10:32:39 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,19 +11,20 @@ void print_times_table(int n)
 int a, b, op;
 	if (n >= 0 && n <= 15)
 {
-	for (a = 0; a <= n; a++)
+for (a = 0; a <= n; a++)
 {
 _putchar(48);
-for (b = 1; b <= n; b++)												{
+for (b = 1; b <= n; b++)
+{
 op = a * b;
 	_putchar(44);
 	_putchar(32);
 	if (op <= 9)
-																{
+{
 _putchar(32);
-															_putchar(32);
-															_putchar(op + 48);
-															}
+_putchar(32);
+_putchar(op + 48);
+	}
 	else if (op <= 99)
 	{
 _putchar(32);
