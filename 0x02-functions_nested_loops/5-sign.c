76d2bfa7fe1af7f6855f88bbbd64664eab9670c4
Raphael Ude <donnick67@gmail.com>
2022-07-05 04:16:53 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * print_sign - Determines if the input number
@@ -11,20 +11,20 @@
  */
 int print_sign(int n)
 {
-		if (n > 0)
-				{
-							_putchar(43);
-									return (1);
-										}
-			else if (n < 0)
-					{
-								_putchar(45);
-										return (-1);
-											}
-				else
-						{
-									_putchar(48);
-											return (0);
-												}
-					_putchar('\n');
+	if (n > 0)
+{
+_putchar(43);
+	return (1);
+}
+	else if (n < 0)
+{
+_putchar(45);
+	return (-1);
+}
+	else
+{
+_putchar(48);
+	return (0);
+}
+_putchar('\n');
 }
